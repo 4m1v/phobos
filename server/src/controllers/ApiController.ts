4638e854f6d9a4d3fc42cb881856b33df917d25f
Mrinaank Sinha <mrinaanks@gmail.com>
2021-08-28 22:06:13 +1000

    Finished basic prototype

--- server/src/controllers/ApiController.ts
+++ server/src/controllers/ApiController.ts
@@ -1,16 +1,16 @@
-import { BodyParam, Post, JsonController } from 'routing-controllers';
-import { OpenAPI } from 'routing-controllers-openapi';
-import { toImage, toSession } from '../json';
-import { InjectRepository } from 'typeorm-typedi-extensions';
-import ImageRepository from '../repositories/ImageRepository';
+import { BodyParam, Post, JsonController } from "routing-controllers";
+import { OpenAPI } from "routing-controllers-openapi";
+import { toImage, toSession } from "../json";
+import { InjectRepository } from "typeorm-typedi-extensions";
+import ImageRepository from "../repositories/ImageRepository";
 // import PhobiaRepository from '../repositories/PhobiaRepository';
-import SessionRepository from '../repositories/SessionRepository';
-import SlideRepository from '../repositories/SlideRepository';
-import { initRecommender, getNextImage, addFeedbackToRecommender } from '../recommender';
+import SessionRepository from "../repositories/SessionRepository";
+import SlideRepository from "../repositories/SlideRepository";
+import { initRecommender, getNextImage, addFeedbackToRecommender, recalculatePredictedRatings } from "../recommender";
 
-import type { Image, Session } from '../../../src/api';
+import type { Image, Session } from "../../../src/api";
 
-@JsonController('/api')
+@JsonController("/api")
 class AnnouncementController {
   @InjectRepository()
   private readonly imageRepository: ImageRepository;
@@ -21,44 +21,53 @@ class AnnouncementController {
   @InjectRepository()
   private readonly slideRepository: SlideRepository;
 
-  @Post('/start')
+  @Post("/start")
   @OpenAPI({
-    summary: 'Called at the star of a session to create a session.',
-    description: 'Output of `{ sessionId: string }`',
+    summary: "Called at the star of a session to create a session.",
+    description: "Output of `{ sessionId: string }`",
   })
   public async start(
-    @BodyParam('fearMin', { required: true }) fearMin: number,
-    @BodyParam('fearMax', { required: true }) fearMax: number,
-    @BodyParam('phobiaId', { required: true }) phobiaId: string,
+    @BodyParam("fearMin", { required: true }) fearMin: number,
+    @BodyParam("fearMax", { required: true }) fearMax: number,
+    @BodyParam("phobiaId", { required: true }) phobiaId: string
   ): Promise<{ sessionId: string }> {
     const session = await this.sessionRepository.createSession(fearMin, fearMax, phobiaId);
-    initRecommender(session);
+    await initRecommender(session);
     return {
       sessionId: session.id,
     };
   }
 
-  @Post('/play')
+  @Post("/test")
   @OpenAPI({
-    summary: 'Called at the start of reviewing an image to get the image.',
-    description: 'Output of `Image` defined in api.ts',
+    summary: "Called at the star of a session to create a session.",
+    description: "Output of `{ sessionId: string }`",
   })
-  public async play(@BodyParam('sessionId', { required: true }) sessionId: string): Promise<Image> {
+  public async test(): Promise<any> {
+    return { result: recalculatePredictedRatings() };
+  }
+
+  @Post("/play")
+  @OpenAPI({
+    summary: "Called at the start of reviewing an image to get the image.",
+    description: "Output of `Image` defined in api.ts",
+  })
+  public async play(@BodyParam("sessionId", { required: true }) sessionId: string): Promise<Image> {
     sessionId;
     const imageId = await getNextImage(sessionId);
     const image = await this.imageRepository.getById(imageId);
     return toImage(image);
   }
 
-  @Post('/feedback')
+  @Post("/feedback")
   @OpenAPI({
-    summary: 'Called at the end of reviewing an image to record how a user responded.',
-    description: 'Output of `{}`',
+    summary: "Called at the end of reviewing an image to record how a user responded.",
+    description: "Output of `{}`",
   })
   public async feedback(
-    @BodyParam('imageId', { required: true }) imageId: string,
-    @BodyParam('sessionId', { required: true }) sessionId: string,
-    @BodyParam('scariness', { required: true }) scariness: number,
+    @BodyParam("imageId", { required: true }) imageId: string,
+    @BodyParam("sessionId", { required: true }) sessionId: string,
+    @BodyParam("scariness", { required: true }) scariness: number
   ): Promise<void> {
     const slideLen = await this.sessionRepository.getSlideLenById(sessionId);
     const slide = await this.slideRepository.createSlide(slideLen, scariness, imageId, sessionId);
@@ -66,12 +75,12 @@ class AnnouncementController {
     addFeedbackToRecommender(sessionId, slide);
   }
 
-  @Post('/result')
+  @Post("/result")
   @OpenAPI({
-    summary: 'Called at the end of a session to review how the user went. ',
-    description: 'Output of `Session` defined in api.ts',
+    summary: "Called at the end of a session to review how the user went. ",
+    description: "Output of `Session` defined in api.ts",
   })
-  public async result(@BodyParam('sessionId', { required: true }) sessionId: string): Promise<Session> {
+  public async result(@BodyParam("sessionId", { required: true }) sessionId: string): Promise<Session> {
     const session = await this.sessionRepository.getByIdWithSlides(sessionId);
     return toSession(session);
   }
--- server/src/recommender.ts
+++ server/src/recommender.ts
@@ -1,68 +1,188 @@
-import { getCustomRepository } from 'typeorm';
-import ImageEntity from './entities/ImageEntity';
-import SessionEntity from './entities/SessionEntity';
-import SlideEntity from './entities/SlideEntity';
-import ImageRepository from './repositories/ImageRepository';
-import PhobiaRepository from './repositories/PhobiaRepository';
+import { Console } from "console";
+import { createConnection, getCustomRepository } from "typeorm";
+import { Slide } from "../../src/api";
+import ImageEntity from "./entities/ImageEntity";
+import SessionEntity from "./entities/SessionEntity";
+import SlideEntity from "./entities/SlideEntity";
+import ImageRepository from "./repositories/ImageRepository";
+import PhobiaRepository from "./repositories/PhobiaRepository";
+import SessionRepository from "./repositories/SessionRepository";
 
 const data = [
   {
-    sessionId: 'haha',
+    sessionId: "haha",
+    similarity: { similarity: 0, features: 0 },
     slides: [
-      { imageId: '0', scariness: 3.2, order: 5 },
-      { imageId: '1', scariness: 1, order: 12 },
-      { imageId: '2', scariness: 5, order: 2 },
+      { imageId: "0", scariness: 3.2, order: 5, adjustedScariness: 3.2 },
+      { imageId: "1", scariness: 1, order: 12, adjustedScariness: 1 },
+      { imageId: "2", scariness: 5, order: 2, adjustedScariness: 5 },
     ],
   },
   {
-    sessionId: 'lmao',
+    sessionId: "lmao",
+    similarity: { similarity: 0, features: 0 },
     slides: [
-      { imageId: '0', scariness: 1.6, order: 3 },
-      { imageId: '1', scariness: 3, order: 5 },
-      { imageId: '2', scariness: 2, order: 1 },
+      { imageId: "0", scariness: 1.6, order: 3, adjustedScariness: 1.6 },
+      { imageId: "1", scariness: 3, order: 5, adjustedScariness: 3 },
+      { imageId: "2", scariness: 2, order: 1, adjustedScariness: 2 },
     ],
   },
   {
-    sessionId: 'rofl',
+    sessionId: "rofl",
+    similarity: { similarity: 0, features: 0 },
     slides: [
-      { imageId: '0', scariness: 4.1, order: 2 },
-      { imageId: '1', scariness: 2.2, order: 1 },
-      { imageId: '2', scariness: 3.8, order: 3 },
+      { imageId: "0", scariness: 4.1, order: 2, adjustedScariness: 4.1 },
+      { imageId: "1", scariness: 2.2, order: 1, adjustedScariness: 2.2 },
+      { imageId: "2", scariness: 3.8, order: 3, adjustedScariness: 3.8 },
     ],
   },
 ];
 
-interface CurrentSession extends SessionEntity {
-  predictedRatings: SlideEntity[];
+interface CurrentSession {
+  id: string;
+  fearMin: number;
+  fearMax: number;
+  phobiaId: string;
+  slides: { imageId: string; scariness: number; order: number; adjustedScariness: number }[];
+  predictedRatings: { imageId: string; scariness: number }[];
 }
 
 let currentSession: CurrentSession = {
-  id: 'really',
+  id: "really",
   fearMin: 3,
   fearMax: 4.5,
-  phobiaId: 'hi',
-  slidesLen: -1,
-  slides: [],
+  phobiaId: "hi",
+  slides: [
+    { imageId: "0", scariness: 3.2, order: 5, adjustedScariness: 3.2 },
+    { imageId: "1", scariness: 2.2, order: 1, adjustedScariness: 2.2 },
+  ],
   predictedRatings: [],
-  phobia: undefined,
-  createdAt: undefined,
-  updatedAt: undefined,
 };
 
-const imageRepository = getCustomRepository(ImageRepository);
+const imageList = ["0", "1", "2"];
 
-export const initRecommender = (session: SessionEntity): void => {
-  currentSession = { ...session, predictedRatings: [] };
+export const initRecommender = async (session: SessionEntity): Promise<void> => {
+  currentSession = {
+    id: session.id,
+    fearMin: session.fearMin,
+    fearMax: session.fearMax,
+    phobiaId: session.phobiaId,
+    slides: [],
+    predictedRatings: [],
+  };
+
+  const connection = await createConnection({
+    type: "sqlite",
+    database: process.env.DB,
+    entities: [__dirname + "../../entities/*.ts"],
+  });
+  await connection.synchronize();
+
+  const imageRepository = getCustomRepository(ImageRepository);
+  const sessionRepository = getCustomRepository(SessionRepository);
+
+  for (const img of await imageRepository.findByPhobiaId(session.phobiaId)) {
+    imageList.push(img.id);
+  }
+
+  const sessions = await sessionRepository.findByPhobiaId(session.phobiaId);
+  sessions.forEach((element) => {
+    const result: { imageId: string; scariness: number; order: number; adjustedScariness: number }[] = [];
+    element.slides.forEach((slide) => {
+      result.push({
+        imageId: slide.imageId,
+        scariness: slide.scariness,
+        order: slide.order,
+        adjustedScariness: slide.scariness,
+      });
+    });
+
+    data.push({ sessionId: element.id, similarity: { similarity: 0, features: 0 }, slides: result });
+  });
 };
 
-// const recalculatePredictedRatings = (): void => {};
+export const recalculatePredictedRatings = (): any => {
+  const viewed: { [x: string]: number } = {};
+  let currentSeshMod = 0;
+
+  // Get what images the current session has viewed, represented in a different way.
+  currentSession.slides.forEach((slide: any) => {
+    viewed[slide.imageId] = slide.scariness;
+    currentSeshMod += slide.scariness * slide.scariness;
+  });
+  currentSeshMod = Math.sqrt(currentSeshMod);
+
+  // Calculate similarity between users
+  data.forEach((session) => {
+    let similarity = 0;
+    let features = 0;
+    let sessionMod = 0;
+
+    session.slides.forEach((slide) => {
+      if (viewed[slide.imageId] !== undefined) {
+        similarity += slide.scariness * viewed[slide.imageId];
+        sessionMod += slide.scariness * slide.scariness;
+        features++;
+      }
+      console.log({ scariness: slide.scariness, imageId: viewed[slide.imageId] });
+    });
+
+    sessionMod = Math.sqrt(sessionMod);
+    if (sessionMod * currentSeshMod !== 0) {
+      similarity /= sessionMod * currentSeshMod;
+    } else {
+      similarity = 0;
+    }
+
+    session.similarity = { similarity: similarity, features: features };
+  });
+
+  // Just some intialisation
+  const predictions: { [imageId: string]: { predictionSum: number; totalSimilarity: number } } = {};
+  imageList.forEach((element) => {
+    predictions[element] = { predictionSum: 0, totalSimilarity: 0 };
+  });
+
+  // Calculating predicted ratings for all images
+  data.forEach((session) => {
+    session.slides.forEach((slide) => {
+      if (viewed[slide.imageId] === undefined) {
+        predictions[slide.imageId].predictionSum += slide.scariness * session.similarity.similarity;
+        predictions[slide.imageId].totalSimilarity += session.similarity.similarity;
+      }
+    });
+  });
+
+  // Storing calculated predictions into the currentSession
+  for (const key in predictions) {
+    if (predictions[key].totalSimilarity !== 0) {
+      currentSession.predictedRatings.push({
+        imageId: key,
+        scariness: predictions[key].predictionSum / predictions[key].totalSimilarity,
+      });
+    }
+  }
+
+  return currentSession.predictedRatings;
+};
 
 export const getNextImage = async (sessionId: string): Promise<string> => {
+  const imageRepository = await createConnection({
+    type: "sqlite",
+    database: process.env.DB,
+    entities: [__dirname + "../../entities/*.ts"],
+  }).then(async (connection) => {
+    await connection.synchronize();
+
+    return getCustomRepository(ImageRepository);
+  });
+
   if (currentSession.slides.length < Number.POSITIVE_INFINITY) {
     // Give them a random easy image.
     const images: ImageEntity[] = await imageRepository.findInScarinessRangeAndPhobiaId(
       currentSession.fearMin - 0.5,
       currentSession.fearMin + 0.5,
+      currentSession.phobiaId
     );
     const index = Math.floor(Math.random() * images.length);
     return images[index].id;
@@ -72,13 +192,18 @@ export const getNextImage = async (sessionId: string): Promise<string> => {
   recalculatePredictedRatings();
 
   // Choose top 1
-  currentSession.predictedRatings.sort((a, b) => b.scariness - a.scariness);
+  currentSession.predictedRatings.sort((a: any, b: any) => b.scariness - a.scariness);
   const image = currentSession.predictedRatings[0];
 
   // Return it
-  return image.id;
+  return image.imageId;
 };
 
 export const addFeedbackToRecommender = (sessionId: string, slide: SlideEntity): void => {
-  currentSession.slides.push(slide);
+  currentSession.slides.push({
+    imageId: slide.imageId,
+    scariness: slide.scariness,
+    order: slide.order,
+    adjustedScariness: slide.scariness,
+  });
 };
