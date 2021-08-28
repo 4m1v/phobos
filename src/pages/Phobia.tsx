d5df9334bb880b58e482c8cc1b115d8f67ac7f73
Michael Gribben <michaelgribbeninfo@gmail.com>
2021-08-28 10:56:13 +1000

    added graphql on frontend

--- src/pages/Phobia.tsx
+++ src/pages/Phobia.tsx
@@ -1,7 +1,8 @@
-import React, { FC, ReactElement } from 'react';
+import { FC, ReactElement } from 'react';
 import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { Typography, makeStyles, createStyles } from '@material-ui/core';
+import { useQuery, gql } from '@apollo/client';
 
 // components
 import BodyContainer from '../components/BodyContainer';
@@ -26,13 +27,32 @@ const useStyles = makeStyles(() =>
   }),
 );
 
+const GET_PHOBIA_IMAGES = gql`
+  query Phobium($phobia: ID!) {
+    Phobium(id: $phobia) {
+      Images {
+        id
+        title
+        url
+        width
+        height
+        scariness
+      }
+    }
+  }
+`;
+
 interface ParamType {
   phobia: string;
 }
 
 const Phobia: FC<Record<string, never>> = (): ReactElement => {
   const { phobia } = useParams<ParamType>();
+  const { loading, error, data } = useQuery(GET_PHOBIA_IMAGES, {
+    variables: { phobia },
+  });
   const classes = useStyles();
+
   return (
     <>
       <Helmet>
@@ -46,6 +66,18 @@ const Phobia: FC<Record<string, never>> = (): ReactElement => {
             <PageTitle title={PAGE_TITLE_PHOBIA} />
             <div>
               <Typography variant="h5">{phobia}</Typography>
+              {loading && <p>Loading...</p>}
+              {error && <p>Error :(</p>}
+              {data && (
+                <div>
+                  {data.Phobium.Images.map(({ id, title, url }: { id: number; title: string; url: string }) => (
+                    <div key={id}>
+                      <Typography variant="h6">{title}</Typography>
+                      <img src={url} width={500} />
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </BodyContainer>
